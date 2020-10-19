fe3e35d9693f04383e22a9c5a94c3cd8cfbbf91d
TingALin <linshaoting6@gmail.com>
2020-10-19 11:40:57 +0800

    C2

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -4,6 +4,9 @@ use codec::{Decode, Encode};
 use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
 use frame_system::ensure_signed;
 use sp_std::collections::BTreeMap;
+use sp_runtime::{RuntimeDebug};
+use orml_traits::{MultiReservableCurrency, MultiCurrency};
+use orml_utilities::with_transaction_result;
 
 #[cfg(test)]
 mod mock;
@@ -14,25 +17,23 @@ mod tests;
 #[cfg(feature ="std")]
 pub use serde::{Deserialize, Serialize};
 
-type BalanceOf<T> = <T as generic_asset::Trait>::Balance;
+type BalanceOf<T> = <<T as Trait>::Currency as MultiCurrency<<T as frame_system::Trait>::AccountId>>::Balance;
+type CurrencyIdOf<T> = <<T as Trait>::Currency as MultiCurrency<<T as frame_system::Trait>::AccountId>>::CurrencyId;
 type DexAccount<T> = <T as frame_system::Trait>::AccountId;
 
 const MINIMUM_LIQUIDITY:u32 = 10_000;
-const INITIAL_SHARES: u32 = 1000;
+const INITIAL_SHARES: u32 = 1_000;
 const FEERATE: u32 = 3; 
-const FEE_RATE_DENOMINATOR: u32 = 1000;
-const DEX_ACCOUNT_ID: DexAccount = AccountId::default(); // to-do
+const FEE_RATE_DENOMINATOR: u32 = 1_000;
+pub const DEX_ACCOUNT_ID: DexAccount = 1;
 
 pub trait Trait: frame_system::Trait {
-	type Event: From<Event<Self>> 
-		+ Into<<Self as frame_system::Trait>::Event>
-		+ Into<<Self as generic_asset::Trait>::Event>;
-	type AssetId: Into<<Self as generic_asset::Trait>::AssetId; //TO- DO
+	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 }
 
 #[cfg_attr(feature="std", derive(Serialize, Deserialize))]
-#[derive(Encode, Decode, Clone, PartialEq, Eq, Debug)]
-pub struct TokenPair<T:Trait> {
+#[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, RuntimeDebug)]
+pub struct TokenPair<T: Trait> {
 	fee_rate: BalanceOf<T>,
 	token_a_pool: BalanceOf<T>,
 	token_b_pool: BalanceOf<T>,
@@ -43,8 +44,8 @@ pub struct TokenPair<T:Trait> {
 
 decl_storage! {
 	trait Store for Module<T: Trait> as TemplateModule {
-		// T::AssetId, T::AssetId use linkedlist???
-		pub PairStructs get(fn pair_structs) map hash(blake2_128_concat) (T::AssetId, T::AssetId) => TokenPair<T>;
+		// TO-DO: CurrencyIdOf<T>, CurrencyIdOf<T> use linkedlist
+		pub PairStructs get(fn pair_structs) map hash(blake2_128_concat) (CurrencyIdOf<T>, CurrencyIdOf<T>) => TokenPair<T>;
 	}
 }
 
@@ -52,14 +53,14 @@ decl_event!(
 	pub enum Event<T> 
 	where 
 		AccountId = <T as frame_system::Trait>::AccountId,
-		<T as Trait>::AssetId,
+		CurrencyId = CurrencyIdOf<T>,
 		Shares = BalanceOf<T>,
 		Balance = BalanceOf<T>,
 	{
-		Initialized(AccountId, AssetId, AssetId, Shares),
-		Swapped(AccountId, Balance, AssetId, Balance),
-		Invested(AccountId, AssetId, AssetId, Shares);
-		Devested(AccountId, AssetId, AssetId, Shares);
+		Initialized(AccountId, CurrencyId, CurrencyId, Shares),
+		Swapped(AccountId, Balance, CurrencyId, Balance),
+		Invested(AccountId, CurrencyId, Shares);
+		Devested(AccountId, CurrencyId, Shares);
 	}
 );
 
@@ -85,7 +86,7 @@ decl_module! {
 		fn deposit_event() = default;
 
 		// to-do: #[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn initialize_exchange(origin, tokenA: T::AssetId, tokenA_amount: T::BalanceOf<T>, tokenB: T::AssetId, tokenB_amount: T::BalanceOf<T>) -> dispatch::DispatchResult{
+		pub fn initialize_exchange(origin, tokenA: CurrencyIdOf<T>, tokenA_amount: T::BalanceOf<T>, tokenB: CurrencyIdOf<T>, tokenB_amount: T::BalanceOf<T>) -> dispatch::DispatchResult{
 			let sender = ensure_signed(origin)?;
 			ensure!(tokenA_amount > 0, Error::<T>::LowTokenAmount);
 			ensure!(tokenB_amount > 0, Error::<T>::LowTokenAmount);
@@ -105,13 +106,14 @@ decl_module! {
 				shares: shares_map,
 			}
 			PairStructs::<T>::insert((tokenA,tokenB) pair);
-			<generic_asset::Module<T>>::make_transfer_with_event(&tokenA, &sender, &DEX_ACCOUNT_ID, tokenA_amount)?;
-			<generic_asset::Module<T>>::make_transfer_with_event(&tokenB, &sender, &DEX_ACCOUNT_ID, tokenB_amount)?;
+			// TO-DO use with_transaction_result(||{Currency::transfer} to replace
+			// <generic_asset::Module<T>>::make_transfer_with_event(&tokenA, &sender, &DEX_ACCOUNT_ID, tokenA_amount)?;
+			// <generic_asset::Module<T>>::make_transfer_with_event(&tokenB, &sender, &DEX_ACCOUNT_ID, tokenB_amount)?;
 			Self::deposit_event(RawEvent::Initialized(sender, tokenA, tokenB, INITIAL_SHARES));
 			Ok(())
 		}
 		#[weight = 10_000]
-		pub fn token_to_token_swap(origin, token_from: T::AssetId, token_from_amount: T::BalanceOf<T>, token_to: T::AssetId,  min_token_received: BalanceOf<T>, receiver: T::AccountId)-> dispatch::DispatchResult{
+		pub fn token_to_token_swap(origin, token_from: CurrencyIdOf<T>, token_from_amount: T::BalanceOf<T>, token_to: CurrencyIdOf<T>,  min_token_received: BalanceOf<T>, receiver: T::AccountId)-> dispatch::DispatchResult{
 			let sender = ensure_signed(origin)?;
 			ensure!(token_from != token_to, Error::<T>::SamePair);		
 			ensure!(PairStructs::<T>::contains_key(token_from, token_to), Error::<T>::PairNotExists);
@@ -130,18 +132,19 @@ decl_module! {
 				pair.token_b_pool = new_token_to_pool;
 				pair.invariant = token_a_pool * token_b_pool;
 			})
-			<generic_asset::Module<T>>::make_transfer_with_event(&token_from, &sender, &DEX_ACCOUNT_ID, token_from_amount)?;
-			<generic_asset::Module<T>>::make_transfer_with_event(&token_to, &DEX_ACCOUNT_ID, &receiver, min_token_received)?;
+			// TO-DO use with_transaction_result(||{Currency::transfer} to replace
+			// <generic_asset::Module<T>>::make_transfer_with_event(&token_from, &sender, &DEX_ACCOUNT_ID, token_from_amount)?;
+			// <generic_asset::Module<T>>::make_transfer_with_event(&token_to, &DEX_ACCOUNT_ID, &receiver, min_token_received)?;
 			Self::deposit_event(RawEvent::Swapped(token_from, token_from_amount, token_to, min_token_received));
 			Ok(())
 		}
-		pub fn invest_liquidity(origin, token_a: T::AssetId, token_b: T::AssetId, shares: BalanceOf<T>)-> dispatch::DispatchResult{
+		pub fn invest_liquidity(origin, token_a: CurrencyIdOf<T>, token_b: CurrencyIdOf<T>, shares: BalanceOf<T>)-> dispatch::DispatchResult{
 			let sender = ensure_signed(origin)?;
 			ensure!(token_a != token_b, Error::<T>::SamePair);
 			ensure!(PairStructs::<T>::contains_key(token_a, token_b), Error::<T>::PairNotExist);
 			let pair = Self::pair_structs(token_a, token_b);
-			let token_b_per_share = pair.token_b_pool/pair.total_shares;
-			let token_b_cost = token_b_per_share * shares;
+			// let token_b_per_share = pair.token_b_pool/pair.total_shares;
+			// let token_b_cost = token_b_per_share * shares;
 			let token_a_per_share = pair.token_a_pool/pair.total_shares;
 			let token_a_cost = token_a_per_share * shares;
 
@@ -153,18 +156,18 @@ decl_module! {
 				};
 				pair.shares.insert(sender.clone(), updated_shares);
 				pair.total_shares += shares;
-				pair.token_b_pool += token_b_cost;
+				//pair.token_b_pool += token_b_cost;
 				pair.token_a_pool += token_a_cost;
-				pair.invariant = pair.ksm_pool * pair.token_a_pool;
+				pair.invariant = pair.token_b_pool * pair.token_a_pool;
 			})
 
-			<generic_asset::Module<T>>::make_transfer_with_event(&token_a, &sender, &DEX_ACCOUNT_ID, token_b_cost)?;
-			<generic_asset::Module<T>>::make_transfer_with_event(&token_b, &sender, &DEX_ACCOUNT_ID, token_a_cost)?;
-			Self::deposit_event(RawEvent::Invested(sender, token_a, token_b, shares));
+			// <generic_asset::Module<T>>::make_transfer_with_event(&token_a, &sender, &DEX_ACCOUNT_ID, token_b_cost)?;
+			// <generic_asset::Module<T>>::make_transfer_with_event(&token_b, &sender, &DEX_ACCOUNT_ID, token_a_cost)?;
+			Self::deposit_event(RawEvent::Invested(sender, token_a, shares));
 			Ok(())
 
 		}
-		pub fn devest_liquidity(origin, token_a: T::AssetId, token_b: T::AssetId, shares_burned: BalanceOf<T>, min_token_a_received: BalanceOf<T>, min_token_b_received: BalanceOf<T> )-> dispatch::DispatchResult{
+		pub fn devest_liquidity(origin, token_a: CurrencyIdOf<T>, token_b: CurrencyIdOf<T>, shares_burned: BalanceOf<T>, min_token_a_received: BalanceOf<T>)-> dispatch::DispatchResult{
 			let sender = ensure_signed(origin)?;
 			ensure!(token_a != token_b, Error::<T>::SamePair);
 			ensure!(PairStructs::<T>::contains_key(token_a, token_b), Error::<T>::PairNotExist);
@@ -176,12 +179,12 @@ decl_module! {
 				Err(Error::<T>::DoesNotOwnShare.into())
 			}
 			let pair = Self::pair_structs(token_a, token_b);
-			let token_b_per_share = pair.token_b_pool/pair.total_shares;
-			let token_b_cost = token_b_per_share * shares_burned;
+			// let token_b_per_share = pair.token_b_pool/pair.total_shares;
+			// let token_b_cost = token_b_per_share * shares_burned;
 			let token_a_per_share = pair.token_a_pool/pair.total_shares;
 			let token_a_cost = token_a_per_share * shares_burned;
 
-			ensure!(token_b_cost >= min_token_b_received, Error::<T>::LowAmountOut);
+			//ensure!(token_b_cost >= min_token_b_received, Error::<T>::LowAmountOut);
 			ensure!(token_a_cost >= min_token_a_received, Error::<T>::LowAmountOut);
 
 			<PairStructs<T>>::mutate(token_a, token_b, |pair| {			
@@ -189,7 +192,7 @@ decl_module! {
 					*share -= shares_burned;
 				}
 				pair.total_shares -= shares_burned;
-				pair.token_b_pool -= token_b_cost;
+				//pair.token_b_pool -= token_b_cost;
 				pair.token_a_pool -= token_a_cost;
 				if pair.total_shares == 0){
 					pair.invariant = 0;
@@ -198,56 +201,12 @@ decl_module! {
 				}
 			});
 
-			<generic_asset::Module<T>>::make_transfer_with_event(&token_a, &DEX_ACCOUNT_ID, &sender, token_b_cost)?;
-			<generic_asset::Module<T>>::make_transfer_with_event(&token_b, &DEX_ACCOUNT_ID, &sender, token_a_cost)?;
-			Self::deposit_event(RawEvent::Devested(sender, token_a, token_b, shares_burned));
+			// <generic_asset::Module<T>>::make_transfer_with_event(&token_a, &DEX_ACCOUNT_ID, &sender, token_b_cost)?;
+			// <generic_asset::Module<T>>::make_transfer_with_event(&token_b, &DEX_ACCOUNT_ID, &sender, token_a_cost)?;
+			Self::deposit_event(RawEvent::Devested(sender, token_a, shares_burned));
 			Ok(())
 		}
 			
 	}
 }
 
-// impl <T: Trait> Module <T> {
-// 	fn skim( ) {}
-// 	fn sync( ) {}
-// 	fn flash_swapping(){}
-// 	fn WTHE(){}
-// 	//time-weighted average prices
-// 	fn update(blance_a:BalanceOf<T>, balance_b:BalanceOf<T>, tokenA_amount: BalanceOf<T>, tokenB_amount: BalanceOf<T>){
-// 		let blockTimestampLast:u128 =; //
-// 		let blockTimestamp = timestamp; //  
-// 		let timeElapsed:u32 = blockTimestamp - blockTimestampLast;
-// 		let mut price_cumulative_a:u128 = 0;
-// 		let mut price_cumulative_b:u128 = 0;
-// 		if timeElapsed > 0 {
-// 			price_cumulative_a += tokenB_amount/tokenA_amount * timeElapsed
-// 			price_cumulative_b += tokenA_amount/tokenB_amount * timeElapsed
-// 		}
-// 	}
-	
-// }
-// // computes the direction and magnitude of the profit-maximizing trade
-// pub fn swap_to_price(token_a_price: BalanceOf<T>, token_b_price: BalanceOf<T>, token_a:AssetId, token_b:AssetId, token_a_max_spend: BalanceOf<T>, token_b_max_spend:BalanceOf<T>, receiver:AccountId){
-// 	//  to-do : tokena/b exits
-// 	// token_a != token_b
-// 	//token_a_price/b > 0
-// 	let pair = Self::pair_structs(token_a, token_b)
-// 	let aToB = pair.token_a_pool * token_b_price / pair.token_b_pool;
-// 	let left: f32; // type: check later
-// 	let right: f32;
-// 	if aToB < token_a_price{ 
-// 		left = (pair.invariant * token_a_price *1000 / token_b_price * 997).into().sqrt();
-// 		right = (pair.token_a_pool * 1000/997).into();	
-// 		max_spend = token_a_max_spend;
-// 	} else {
-// 		left = (pair.invariant * token_b_price *1000 / token_a_price * 997).into().sqrt();
-// 		right = (pair.token_b_pool * 1000/997).into();
-// 		max_spend = token_b_max_spend;
-// 	}
-// 	// compute the amount that must be sent to move the price to the profit-maximizing price
-// 	let amount_in = left - right; // tokena or b????
-// 	if amount_in > max_spend{
-// 		let amount_in = max_spend;
-// 	}
-// 	// return mount_in
-// }
